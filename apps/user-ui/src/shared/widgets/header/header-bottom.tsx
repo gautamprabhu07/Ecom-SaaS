e4d161afc32c6c31118f2fe361c2fa3136ad0141
gautamprabhu07 <gautamprabhumit@gmail.com>
2026-07-01 20:30:11 +0530

    improving dynamic loading

--- apps/user-ui/src/shared/widgets/header/header-bottom.tsx
+++ apps/user-ui/src/shared/widgets/header/header-bottom.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { AlignLeft, ChevronDown, Search, CircleUserRound, ShoppingCartIcon, Heart } from 'lucide-react';
+import { AlignLeft, ChevronDown,  CircleUserRound, ShoppingCartIcon, Heart } from 'lucide-react';
 import Link from 'next/link';
 import { NavItem, navItems } from '../../../configs/constants';
 import React, { useEffect, useState } from 'react'
@@ -8,7 +8,7 @@ import useUser from '../../../hooks/useUser';
 const HeaderBottom = () => {
   const [show, setShow] = useState(false);
   const [isSticky, setIsSticky] = useState(false);
-  const {user}=useUser();
+  const {user, isLoading}=useUser();
   console.log("user",user);
 
   useEffect(() => {
@@ -54,37 +54,37 @@ const HeaderBottom = () => {
 
         {/* Sticky Search + Icons */}
         {isSticky && (
-          <div className="flex items-center gap-5 ml-auto">
-            {/* Search */}
-            <div className="flex items-center relative">
-              <input
-                type="text"
-                placeholder="Search products..."
-                className="pl-4 pr-10 py-1.5 text-sm text-gray-800 rounded-lg border-2 border-blue-500 focus:outline-none w-48"
-              />
-              <div className="absolute right-0 h-full px-2.5 flex items-center bg-blue-500 rounded-r-lg cursor-pointer">
-                <Search className="w-4 h-4 text-white" />
-              </div>
-            </div>
-
-            {/* Icons */}
-            <Link href="/login" className="flex flex-col items-center gap-0.5 hover:text-amber-400 transition">
-              <CircleUserRound className="w-5 h-5" />
-              <span className="text-[10px]">Sign in</span>
+          <div className="flex items-center gap-5 shrink-0">
+          {/* Account */}
+          {!isLoading && user ? (
+            <>
+            <Link href="/profile" className="flex flex-col items-center gap-0.5 hover:text-amber-400 transition">
+            <CircleUserRound className=" h-5" /></Link>
+            <Link href="/profile">
+            <span className="text-xs leading-none">Hello{" "}</span>
+            <span className="text-xs leading-none">{user?.name?.split(" ")[0]}</span>
             </Link>
+            </>):(
+              <>
+          <Link href="/login" className="flex flex-col items-center gap-0.5 hover:text-amber-400 transition">
+            <CircleUserRound className="w-5 h-5" />
+            <span className="text-xs leading-none">{isLoading?"...": "Sign in"}</span>
+          </Link></>)}
 
-            <Link href="/wishlist" className="relative flex flex-col items-center gap-0.5 hover:text-amber-400 transition">
-              <Heart className="w-5 h-5" />
-              <span className="text-[10px]">Wishlist</span>
-              <span className="absolute -top-1.5 -right-2 bg-blue-500 text-white text-[10px] w-4 h-4 flex items-center justify-center rounded-full">0</span>
-            </Link>
+          {/* Wishlist */}
+          <Link href="/wishlist" className="relative flex flex-col items-center gap-0.5 hover:text-amber-400 transition">
+            <Heart className="w-5 h-5" />
+            <span className="text-xs leading-none">Wishlist</span>
+            <span className="absolute -top-1.5 -right-2 bg-blue-500 text-white text-[10px] w-4 h-4 flex items-center justify-center rounded-full">0</span>
+          </Link>
 
-            <Link href="/cart" className="relative flex flex-col items-center gap-0.5 hover:text-amber-400 transition">
-              <ShoppingCartIcon className="w-5 h-5" />
-              <span className="text-[10px]">Cart</span>
-              <span className="absolute -top-1.5 -right-2 bg-blue-500 text-white text-[10px] w-4 h-4 flex items-center justify-center rounded-full">9</span>
-            </Link>
-          </div>
+          {/* Cart */}
+          <Link href="/cart" className="relative flex flex-col items-center gap-0.5 hover:text-amber-400 transition">
+            <ShoppingCartIcon className="w-5 h-5" />
+            <span className="text-xs leading-none">Cart</span>
+            <span className="absolute -top-1.5 -right-2 bg-blue-500 text-white text-[10px] w-4 h-4 flex items-center justify-center rounded-full">9</span>
+          </Link>
+        </div>
         )}
 
       </div>
